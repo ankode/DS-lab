7fb4c8d82d1c7cf15bf5b9f95587137b25316293
Ankush Anshuman <ankush3107@gmail.com>
2016-08-14 11:11:11 +0530

    question 2 to 6 is added.

---
+++ week2/biggest.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int biggest(int a[],int n)
+{
+	int max=0;
+    for(int i=0;i<n;i++)
+    {
+			if(a[i]>=max)
+			max=a[i];
+	}
+    return max;
+}
+
+int main()
+{
+    int n,a[100];
+    printf("enter the number of element\n");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        
+        printf("biggest is %d", biggest(a,n));
+    return 0;
+}
